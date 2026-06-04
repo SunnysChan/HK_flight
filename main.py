7d30db45323ee7e520d4bc0122048a7b6e8fc8ec
SunnysChan <152602656+SunnysChan@users.noreply.github.com>
2026-06-04 14:23:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,12 +39,12 @@ def extract_destinations(flight_data):
 
     return destinations
 
-target={'BKK': ['VTBS','Bangkok'],'PEK': ['ZBAA','Beijing'],'TFU': ['ZUUU','Chengdu'],'DAD': ['VVDN','Da Nang'],'DEL': ['VIDP','Delhi'],
-        'FUK': ['RJFF','Fukuoka'],'HAK': ['ZJHK','Haikou'],'HND': ['RJTT','Haneda'],'HGH': ['ZSHC','Hangzhou'],'SGN': ['VVTS','Ho Chi Minh'],
+target={'BKK': ['VTBS','Bang Kok'],'PEK': ['ZBAA','BeiJing'],'TFU': ['ZUUU','ChengDu'],'DAD': ['VVDN','Da Nang'],'DEL': ['VIDP','Delhi'],
+        'FUK': ['RJFF','Fukuoka'],'HAK': ['ZJHK','HaiKou'],'HND': ['RJTT','Haneda'],'HGH': ['ZSHC','HangZhou'],'SGN': ['VVTS','Ho Chi Minh'],
         'ICN': ['RKSI','Incheon'],'CGK': ['WIII','Jakarta'],'KIX': ['RJBB','Kansai'],'KHH': ['RCKH','Kaohsiung'],'KUL': ['WMKK','Kuala Lumpur'],
-        'MNL': ['RPLL','Manila'],'OKA': ['ROAH','Naha'],'NRT': ['RJAA','Narita'],'SHA': ['ZSSS','Shanghai Hongqiao'],'PVG': ['ZSPD','Shanghai Pudong'],
-        'SIN': ['WSSS','Singapore'],'TPE': ['RCTP','Taipei'],'PKX':['ZBAD','Beijing DaXing'],'HAN':['VVNB','Ha Noi'],'NKG':['ZSNJ','Nanjing'],
-       'PQC':['VVPQ','Phu']}
+        'MNL': ['RPLL','Manila'],'OKA': ['ROAH','Naha'],'NRT': ['RJAA','Narita'],'SHA': ['ZSSS','ShangHai HongQiao'],'PVG': ['ZSPD','ShangHai PuDong'],
+        'SIN': ['WSSS','Singapore'],'TPE': ['RCTP','Taipei'],'PKX':['ZBAD','BeiJing DaXing'],'HAN':['VVNB','Ha Noi'],'NKG':['ZSNJ','NanJing'],
+        'PQC':['VVPQ','Phu'],'CAN':['ZGGG','GuangZhou']}
 
 try:
     data = get_departures_by_date()
